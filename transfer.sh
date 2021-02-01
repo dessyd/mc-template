c740b5709749dd13242f06ae966bf0206699dce7
Dominique <dominique@i9.local>
2021-02-01 16:18:16 +0100

    Made generic

--- transfer.sh
+++ transfer.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+USER=some-user
+HOST=some-host
+SSH_PORT=22
+SSH_IDENTITY=""
 CONTAINER=${PWD##*/}_backups_1
 SOURCE="~/mc/Biomes/mc-backups/world-20210201-105513.tgz"
 echo "Source: $SOURCE"
 
 docker-compose stop mc
 echo "Transfering world ..."
-ssh -i ~/.ssh/gurga -p 2022 dominique@gurga.local "cat $SOURCE" | docker exec -i $CONTAINER tar -C /data -xzf - 
+ssh -i $SSH_IDENTITY -p $SSH_PORT $USER@$HOST "cat $SOURCE" | docker exec -i $CONTAINER tar -C /data -xzf - 
 echo "... done"
 docker-compose start mc
