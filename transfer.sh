c8286f7f5fbb7319baf8fee486d54e9e5efce852
Dominique <dominique@i9.local>
2021-02-01 16:01:46 +0100

    Make CONTAINEr name generic

--- transfer.sh
+++ transfer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+CONTAINER=${PWD##*/}_backups_1
 SOURCE="~/mc/Biomes/mc-backups/world-20210201-105513.tgz"
 echo "Source: $SOURCE"
 
 docker-compose stop mc
 echo "Transfering world ..."
-ssh -i ~/.ssh/gurga -p 2022 dominique@gurga.local "cat $SOURCE" | docker exec -i mc-template_backups_1 tar -C /data -xzf - 
+ssh -i ~/.ssh/gurga -p 2022 dominique@gurga.local "cat $SOURCE" | docker exec -i $CONTAINER tar -C /data -xzf - 
 echo "... done"
 docker-compose start mc
